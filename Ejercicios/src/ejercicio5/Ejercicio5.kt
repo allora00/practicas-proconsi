0d5b6505972be59b18c632e85372202652b58ce7
Alberto Llorente <91263653+allora00@users.noreply.github.com>
2025-10-16 09:47:26 +0200

    Add files via upload
    Ejercicio 5 hecho

---
+++ Ejercicios/src/ejercicio5/Ejercicio5.kt
@@ -0,0 +1,17 @@
+package ejercicio5
+
+fun main(){
+    val stdIn = "Bread$$##12.5$$##10";
+    val valores = ItemSeparator(stdIn)
+
+    println("Nombre: " + valores.name)
+    println("Precio: " + valores.price)
+    println("Cantidad: " + valores.quantity)
+}
+
+class ItemSeparator(val valores: String){
+    private val aux: List<String> = valores.split("$$##")
+    var name: String = aux[0]
+    var price: Double = aux[1].toDouble()
+    var quantity: Int = aux[2].toInt()
+}
