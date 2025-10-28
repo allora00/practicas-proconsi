b811e95fb51e258959d2368eba3f229c06712521
Alberto Llorente <allora00@estudiantes.unileon.es>
2025-10-28 11:24:27 +0100

    ejercicio 3 repasado

--- Ejercicios/src/ejercicio3/Ejercicio3.kt
+++ Ejercicios/src/ejercicio3/Ejercicio3.kt
@@ -3,6 +3,7 @@ package ejercicio3
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
+import kotlin.math.absoluteValue
 
 fun main(){
 
@@ -26,8 +27,8 @@ fun main(){
 }
 
 fun diferencia(fechaFormat1: LocalDate, fechaFormat2: LocalDate) {
-    val diferenca = ChronoUnit.DAYS.between(fechaFormat1, fechaFormat2)
-    println("Hay una diferencia de $diferenca dias")
+    val diferenca = ChronoUnit.DAYS.between(fechaFormat1, fechaFormat2).absoluteValue
+    println("\n--- DIFERENCIA ---\nHay una diferencia de $diferenca dias\n")
 }
 
 fun inicioFin(fechaFormat1: LocalDate, fechaFormat2: LocalDate) {
@@ -35,10 +36,12 @@ fun inicioFin(fechaFormat1: LocalDate, fechaFormat2: LocalDate) {
     val fin1 = LocalDate.of(fechaFormat1.year, 12, 31)
     val inicio2 = LocalDate.of(fechaFormat2.year, 1, 1)
     val fin2 = LocalDate.of(fechaFormat2.year, 12, 31)
+    println("--- INICIO/FIN ---")
     print("Inicio 1ºfecha: $inicio1     ")
     println("Fin 1ºfecha: $fin1")
     print("Inicio 2ºfecha: $inicio2     ")
     println("Fin 2ºfecha: $fin2")
+    println("")
 }
 
 fun dias(fechaFormat1: LocalDate, fechaFormat2: LocalDate) {
@@ -46,7 +49,7 @@ fun dias(fechaFormat1: LocalDate, fechaFormat2: LocalDate) {
     val dias1 = ChronoUnit.DAYS.between(inicio1, fechaFormat1)
     val inicio2 = LocalDate.of(fechaFormat2.year, 1, 1)
     val dias2 = ChronoUnit.DAYS.between(inicio2, fechaFormat2)
-    println("Dias 1º fecha: $dias1  Dias 2º fecha: $dias2")
+    println("--- DIAS HASTA FIN DE AÑO ---\nDias 1º fecha: $dias1  Dias 2º fecha: $dias2\n")
 }
 
 fun semanas(fechaFormat1: LocalDate, fechaFormat2: LocalDate) {
@@ -54,5 +57,5 @@ fun semanas(fechaFormat1: LocalDate, fechaFormat2: LocalDate) {
     val dias1 = ChronoUnit.WEEKS.between(inicio1, fechaFormat1)
     val inicio2 = LocalDate.of(fechaFormat2.year, 1, 1)
     val dias2 = ChronoUnit.WEEKS.between(inicio2, fechaFormat2)
-    println("Semanas 1º fecha: $dias1  Semanas 2º fecha: $dias2")
+    println("---SEMANAS HATA FIN DE AÑO ---\nSemanas 1º fecha: $dias1  Semanas 2º fecha: $dias2\n")
 }
